56ead1480c2f58e9a02a70cdd0f66ea5168ecab4
Garuka404 <garukagimhana@gmail.com>
2024-10-27 21:56:29 +0530

    Add functionality to calculate annual bonus and loan amount
    - Implemented option to calculate annual bonus based on employee salary brackets
      - Added bonus calculation with specific percentages for salary ranges
    - Added loan amount calculation based on employee salary, term, and interest rate
      - Used a loop to compute the power term in the loan formula
      - Added eligibility checks for loan conditions
    - Included input validation for salary and loan term restrictions
    - Improved UI display for each calculation option

--- src/Main.java
+++ src/Main.java
@@ -65,10 +65,66 @@ public class Main {
 
         } else if (option==2) {
 
+            System.out.println("\n\n------------------------------------------------------------");
+            System.out.println("|                    Calculate Annual Bonus                 |");
+            System.out.println("------------------------------------------------------------");
+            System.out.println();
+            System.out.print("In put Employee name    - ");
+            String name=scan.next();
+            System.out.print("In put Employee Salary  - ");
+            double salary= scan.nextDouble();
 
+            if(salary<100000){
+                bonus+=5000;;
+            }else if (salary>=100000 && salary<=199999) {
+                bonus+=(salary*10)/100;
+            }
+            else if (salary>=200000 && salary<=299999) {
+                bonus+=(salary*15)/100;
+            }else if (salary>=300000 && salary<=199999) {
+                bonus+=(salary*20)/100;
+            }else if (salary>=400000) {
+                bonus+=(salary*35)/100;
+            }
+            else{
+                System.out.println("!!!!!Invalid Salary!!!!!");
+            }
+            System.out.println("\nAnnual bonus   - "+bonus);
 
         } else if (option==3) {
+            double loan; int months; double rate;
+            System.out.println("\n\n------------------------------------------------------------");
+            System.out.println("|                    Calculate Loan Amount                  |");
+            System.out.println("------------------------------------------------------------");
+            System.out.println();
+            System.out.print("In put Employee name    - ");
+            String name=scan.next();
+            System.out.print("In put Employee Salary  - ");
+            double salary= scan.nextDouble();
+            if(salary>50000){
+                System.out.print("Enter number of year : ");
+                int year=scan.nextInt();
 
+                if(year<=5){
+                    months=year*12;
+
+
+                    int y=1;
+
+                    double power=1.0125;
+                    while(y<months-1){
+                        power=power*1.0125;
+                        y++;
+                    }
+                    System.out.println(power);
+                   double h=(1/(1+power))/0.0125;
+                    System.out.println(h);
+                    loan=(0.6*salary)*((1-h));
+                    System.out.print("You can get loan amount : "+loan);
+
+                }
+
+            }
         }
         else{
             System.out.println("\n!!!!!!!!!!Invalid Choosed Number!!!!!!!!!!!! ");
